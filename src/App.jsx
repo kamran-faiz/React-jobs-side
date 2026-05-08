9ea274ac093202c2adc0c435f6b1ed3af66b869b
kamran/faiz <faizkamran961@gmail.com>
2026-05-09 00:07:15 +0500

    Created a separate component for view jobs to keep app.jsx clean and tidy

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards";
 import JobListings from "./components/JobListings";
+import ViewAllJobs from "./components/ViewAllJobs"
+
+
+
 const App = () => {
   return (
     <>
@@ -11,6 +15,7 @@ const App = () => {
       <Hero />
       <HomeCards />
       <JobListings />
+      <ViewAllJobs />
       
      
 
@@ -20,14 +25,7 @@ const App = () => {
      
       
 
-      <section className="m-auto max-w-lg my-10 px-6">
-        <a
-          href="jobs.html"
-          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-        >
-          View All Jobs
-        </a>
-      </section>
+     
     </>
   );
 };
--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -22,12 +22,7 @@ const jobListing = ({job}) => {
                     <i className="fa-solid fa-location-dot text-lg"></i>
                     {job.location}
                   </div>
-                  <a
-                    href={'/job/${job.id}'}
-                    className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
-                  >
-                    Read More
-                  </a>
+                  
                 </div>
               </div>
             </div>
