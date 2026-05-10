3930cbebdc4878fe66c669d5ad5d27d04a8c7a08
kamran/faiz <faizkamran961@gmail.com>
2026-05-11 01:52:08 +0500

    Add job compalte now we can add te job using post method

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,29 @@ import NotFound from './pages/NotFound';
 import AddJobPage from './pages/AddJobPage';
 import JobPage, {jobLoader} from './pages/JobPage';
 
+
+
+
+const App = () => {
+
+  const addJob =async (newJob) => {
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(newJob)
+    })
+    return;
+  
+};
+
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
   <Route index element={<HomePage/>}/>
   <Route path="jobs" element={<JobsPage />} />
-  <Route path="add-job" element={<AddJobPage />} />
+  <Route path="add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
    <Route path="jobs/:id" element={<JobPage/>} loader={jobLoader}/>
   <Route path="*" element={<NotFound />} />
  
@@ -24,9 +41,6 @@ const router = createBrowserRouter(
   )
 );
 
-
-
-const App = () => {
   return <RouterProvider router={router} />
 };
 
