a839be4237ca2f5c582e3c3b4796a0aaae6eaa4f
kamran/faiz <faizkamran961@gmail.com>
2026-05-11 23:01:24 +0500

    feat: implement delete job functionality in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ import JobPage, {jobLoader} from './pages/JobPage';
 
 
 const App = () => {
-
+  // Add new job
   const addJob =async (newJob) => {
     const res = await fetch('/api/jobs', {
       method: 'POST',
@@ -27,14 +27,22 @@ const App = () => {
     return;
   
 };
+  // delete job
 
+  const deleteJob = async(id) => {
+      const res = await fetch(`/api/jobs/${id}`, {
+        method: 'DELETE',
+        
+      });
+      return;
+  };
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
   <Route index element={<HomePage/>}/>
   <Route path="jobs" element={<JobsPage />} />
-  <Route path="add-job" element={<AddJobPage addJobSubmit={addJob}/>} />
-   <Route path="jobs/:id" element={<JobPage/>} loader={jobLoader}/>
+  <Route path="add-job" element={<AddJobPage  addJobSubmit={addJob}/>} />
+   <Route path="jobs/:id" element={<JobPage  deleteJob={deleteJob}/>} loader={jobLoader}/>
   <Route path="*" element={<NotFound />} />
  
   </Route>
