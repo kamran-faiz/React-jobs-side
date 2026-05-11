78861b93d010a1bdbf1c152944b6b42853165677
kamran/faiz <faizkamran961@gmail.com>
2026-05-11 23:50:20 +0500

    feat: add updateJob function and EditJobPage route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,16 @@ const App = () => {
       });
       return;
   };
+  const updateJob = async(job) => {
+         const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      body: JSON.stringify(job)
+    })
+    return;
+  }
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path="/" element={<MainLayout/>}>
@@ -44,7 +54,7 @@ const router = createBrowserRouter(
   <Route path="jobs" element={<JobsPage />} />
   <Route path="add-job" element={<AddJobPage  addJobSubmit={addJob}/>} />
    <Route path="jobs/:id" element={<JobPage  deleteJob={deleteJob}/>} loader={jobLoader}/>
-   <Route path="edit-job/:id" element={<EditJobPage  />} loader={jobLoader}/>
+   <Route path="edit-job/:id" element={<EditJobPage updateJobSubmit={updateJob}  />} loader={jobLoader}/>
   <Route path="*" element={<NotFound />} />
  
   </Route>
